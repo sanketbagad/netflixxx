5130dfd910a25fb25c741831b1447a6fe2c20b6a
Sanket Bagad <alencolins@gmail.com>
2023-02-22 19:06:31 +0530

    Pallu

--- server.js
+++ server.js
@@ -12,7 +12,14 @@ dotenv.config();
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({
+    limit: '500mb'
+}));
+
+app.use(express.urlencoded({
+    limit: '500mb',
+    extended: true
+}));
 
 connectDB();
 
