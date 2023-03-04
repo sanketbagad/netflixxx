d713df097533586ae61ff5987b6bf623858c0d7d
Sanket Bagad <alencolins@gmail.com>
2023-03-04 16:04:39 +0530

    afsd

--- Routes/movieroutes2.js
+++ Routes/movieroutes2.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { deleteAllMovies, topRatedMovies, getRandomMovies } from "../Controllers/MoviesController.js";
+import { deleteAllMovies, topRatedMovies, getRandomMovies, createMovieReview } from "../Controllers/MoviesController.js";
 import { protect, admin } from "../middleware/authMiddleware.js";
 const router = express.Router();
 
 router.delete("/deleteAllMovies", deleteAllMovies);
-
+router.post("/reviews", protect, createMovieReview);
 router.get("/topRatedMovies", topRatedMovies);
 router.get("/randomMovies", getRandomMovies);
 
